164bf849731086f97e247bd8e6b09bdb32c3e167
Vitaly Pronkin <vitaly.pronkin@monitorbm.com>
2018-04-26 16:47:21 +1200

    simple shadows are back

--- src/app/CircularShadow.cpp
+++ src/app/CircularShadow.cpp
@@ -10,19 +10,30 @@ CircularShadow::CircularShadow() {
 }
 
 void CircularShadow::draw(NVGcontext *vg) {
-	return;
 	if (opacity < 0.0)
 		return;
 
+	// nvgBeginPath(vg);
+	// nvgRect(vg, -blurRadius, -blurRadius, box.size.x + 2*blurRadius, box.size.y + 2*blurRadius);
+	// Vec center = box.size.div(2.0);
+	// float radius = center.x;
+	// NVGcolor icol = nvgRGBAf(0.0, 0.0, 0.0, opacity);
+	// NVGcolor ocol = nvgRGBAf(0.0, 0.0, 0.0, 0.0);
+	// NVGpaint paint = nvgRadialGradient(vg, center.x, center.y, radius - blurRadius, radius, icol, ocol);
+	// nvgFillPaint(vg, paint);
+	// nvgFill(vg);
+
 	nvgBeginPath(vg);
-	nvgRect(vg, -blurRadius, -blurRadius, box.size.x + 2*blurRadius, box.size.y + 2*blurRadius);
+	// nvgRect(vg, -blurRadius, -blurRadius, box.size.x + 2*blurRadius, box.size.y + 2*blurRadius);
 	Vec center = box.size.div(2.0);
 	float radius = center.x;
-	NVGcolor icol = nvgRGBAf(0.0, 0.0, 0.0, opacity);
-	NVGcolor ocol = nvgRGBAf(0.0, 0.0, 0.0, 0.0);
-	NVGpaint paint = nvgRadialGradient(vg, center.x, center.y, radius - blurRadius, radius, icol, ocol);
-	nvgFillPaint(vg, paint);
-	nvgFill(vg);
+	nvgCircle(vg, center.x, center.y, radius);
+	// NVGcolor icol = nvgRGBAf(0.0, 0.0, 0.0, opacity);
+	// NVGcolor ocol = nvgRGBAf(0.0, 0.0, 0.0, 0.0);
+	// NVGpaint paint = nvgRadialGradient(vg, center.x, center.y, radius - blurRadius, radius, icol, ocol);
+	nvgFillColor(vg, nvgRGBAf(0.0, 0.0, 0.0, opacity));
+	// nvgFillPaint(vg, paint);
+	nvgFill(vg);	
 }
 
 
