6645df421f6c9fe7b7e79d9f94ff68dbb9f78abb
Vitaly Pronkin <pronvit@me.com>
2018-04-27 02:06:12 +1200

    makefile fixes

--- plugin.mk
+++ plugin.mk
@@ -9,7 +9,8 @@ endif
 FLAGS += -DSLUG=$(SLUG)
 FLAGS += -fPIC
 FLAGS += -I$(RACK_DIR)/include -I$(RACK_DIR)/dep/include \
-         -I$(RACK_DIR)/dep/nanovg/src -I$(RACK_DIR)/dep/nanosvg/src -I$(RACK_DIR)/dep/oui-blendish
+         -I$(RACK_DIR)/dep/nanovg/src -I$(RACK_DIR)/dep/nanosvg/src -I$(RACK_DIR)/dep/oui-blendish \
+         -I$(RACK_DIR)/dep/osdialog
 
 
 include $(RACK_DIR)/arch.mk
