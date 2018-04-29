e820779a209dab236bf6b7f72cd259a164a28166
Vitaly Pronkin <vitaly.pronkin@monitorbm.com>
2018-04-30 11:11:46 +1200

    added status flags to the plugin list

--- plugin-list.mk
+++ plugin-list.mk
@@ -2,7 +2,7 @@ SHELL = /bin/bash
 
 plugin:
 	@mkdir -p plugins
-	@[ ! -e plugins/$(DIR) ] && true || ( echo --- ; echo -e "Plugin folder plugins/$(DIR) already exists and will be overwritten. Press \033[1my\033[0m to proceed or any other key to skip this plugin." ; echo --- ; read -n 1 R; [ "$$R" = "y" ] )
+	@[ ! -e plugins/$(DIR) ] && true || ( echo -e "---\nPlugin folder plugins/$(DIR) already exists and will be overwritten. Press \033[1my\033[0m to proceed or any other key to skip this plugin.\n---" ; read -n 1 R; [ "$$R" = "y" ] )
 	rm -rf plugins/$(DIR)
 ifeq (,$(findstring http,$(URL)))
 	git clone http://github.com/$(URL) plugins/$(DIR) --recursive
@@ -15,9 +15,10 @@ endif
 	@echo ---
 
 list-plugins:
-	@awk -F ' *\\| *' '{ if(match($$1,"^#")) next; print "\033[1m"$$1"\033[0m" "\n\t" $$3; if(index($$2,"http")) print "\t"$$2 ; else print "\thttp://github.com/"$$2 ; print ""; }' plugin-list.txt 
+	@awk -F ' *\\| *' '{ if(match($$1,"^#")) next; print "[" $$3 "] \033[1m"$$1"\033[0m" "\n\t" $$4; if(index($$2,"http")) print "\t"$$2 ; else print "\thttp://github.com/"$$2 ; print ""; }' plugin-list.txt 
 	@echo ---
-	@echo "Type \"make +\033[1mPluginSlug\033[0m +\033[1mPluginSlug\033[0m ...\" to install plugins (note the plus signs)."
+	@echo -e "[?] - Untested   [-] - Broken   [!] - Has issues   [~] - Looks OK   [+] - Thoroughly tested and optimised (if needed)"
+	@echo -e "Type \"make +\033[1mPluginSlug\033[0m +\033[1mPluginSlug\033[0m ...\" to install plugins (note the plus signs)."
 	@echo ---
 
 rebuild-plugins:
