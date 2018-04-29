572146d857be8bc2712adb788df1051cac7ce9ab
Vitaly Pronkin <pronvit@me.com>
2018-04-30 03:55:16 +1200

    fixing plugin list makefile

--- plugin-list.mk
+++ plugin-list.mk
@@ -1,12 +1,15 @@
+SHELL = /bin/bash
+
 plugin:
 	@mkdir -p plugins
-	@[ -e plugins/$(DIR) ] && ( echo --- ; echo "Plugin folder plugins/$(DIR) already exists and will be overwritten. Press \033[1my\033[0m to proceed or any other key to skip this plugin." ; echo --- ; read -n1 R; [ "$$R" = "y" ] && rm -rf plugins/$(DIR) || false ) || true
+	@[ ! -e plugins/$(DIR) ] && true || ( echo --- ; echo -e "Plugin folder plugins/$(DIR) already exists and will be overwritten. Press \033[1my\033[0m to proceed or any other key to skip this plugin." ; echo --- ; read -n 1 R; [ "$$R" = "y" ] )
+	rm -rf plugins/$(DIR)
 ifeq (,$(findstring http,$(URL)))
 	git clone http://github.com/$(URL) plugins/$(DIR) --recursive
 else
 	git clone $(URL) plugins/$(DIR) --recursive
 endif
-	make -C plugins/$(DIR)
+	$(MAKE) -C plugins/$(DIR)
 	@echo ---
 	@echo Plugin $(DIR) built.
 	@echo ---
@@ -23,7 +26,7 @@ rebuild-plugins:
 #ALL_PLUGINS = $(shell awk -F ' *\\| *' '{ if(match($$1,"^\#")) next; printf "+%s ", $$1 }' plugin-list.txt)
 
 +all:
-	awk -F ' *\\| *' '{ if(match($$1,"^#")) next; print "+" $$1 }' plugin-list.txt | xargs -o make -i
+	for slug in $$(awk -F ' *\\| *' '{ if(match($$1,"^#")) next; print "+" $$1 }' plugin-list.txt) ; do $(MAKE) $$slug ; done
 
 # Special case as it actually builds two plugins
 +Fundamental:
